6743176717dc3ace0c56adecb2aaad44ad1aa6a1
Admin <abdutokt2004>
2023-05-22 22:14:01 +0600

    Initial commit

--- src/main/java/com/example/telegrambotapi/service/impl/CurrencyServiceImpl.java
+++ src/main/java/com/example/telegrambotapi/service/impl/CurrencyServiceImpl.java
@@ -6,7 +6,11 @@ import com.example.telegrambotapi.modals.enums.Currencies;
 import com.example.telegrambotapi.repository.CurrencyRepository;
 import com.example.telegrambotapi.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.json.JSONObject;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,6 +18,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Objects;
 import java.util.Scanner;
 
 @Service
@@ -30,27 +35,15 @@ public class CurrencyServiceImpl implements CurrencyService {
 
     @Override
     public String getCurrencyRates(CurrencyDto currencyDto, String message) throws IOException, ParseException {
-        URL url = new URL("https://api.apilayer.com/currency_data/convert?to=KZT&from=USD&amount=5");
-        Scanner scanner = new Scanner((InputStream) url.getContent());
-        String result = "";
-        while (scanner.hasNext()) {
-            result += scanner.nextLine();
-        }
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
 
-        JSONObject object = new JSONObject(result);
-
-        currencyDto.setId(object.getLong("Id"));
-        currencyDto.setDate(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(object.getString("Date")));
-        currencyDto.setHistorical(object.getBoolean("historical"));
-        currencyDto.setAmount(object.getDouble("amount"));
-        currencyDto.setFromCurrencies(object.getEnum(Currencies.class, "from"));
-        currencyDto.setToCurrencies(object.getEnum(Currencies.class, "to"));
-        currencyDto.setResult(object.getDouble("result"));
-        currencyDto.setSuccess(object.getBoolean("success"));
-
-        return "Official rate of KZT to " + currencyDto.getToCurrencies() + "\n" +
-                "on the date: " + getFormatDate(currencyDto) + "\n" +
-                "is: " + currencyDto.getQuote() + " USD result: " + currencyDto.getResult();
+        Request request = new Request.Builder()
+                .url("https://api.apilayer.com/exchangerates_data/convert?to=KZT&from=USD&amount=5")
+                .addHeader("apikey", "qFMCvOjKbiP1bqMlT0WbCol5E1eKBonZ")
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
+        return Objects.requireNonNull(response.body()).string();
     }
 
     private static String getFormatDate(CurrencyDto currencyDto) {
